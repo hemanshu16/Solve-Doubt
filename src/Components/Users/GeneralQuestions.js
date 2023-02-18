d2623b0a525dd401e434ef7f50f31110c3115b0a
hemanshu16 <84268731+hemanshu16@users.noreply.github.com>
2023-02-18 18:04:15 +0530

    generalquestion section of user done

--- src/Components/Users/GeneralQuestions.js
+++ src/Components/Users/GeneralQuestions.js
@@ -1,23 +1,138 @@
-import React from "react"; 
+import React from "react";
 
-export default function(props)
-{
+export default function GeneralQuestions (props) {
     return (
         <>
-          <div className="container">
-                    <div className="row d-flex flex-column align-items-center ">
-                  <div className="col-8">
-                  <div className="card" style="width: 18rem;">
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    
-  </div>
-</div>
-                    </div>                
+            <div className="container">
+                <div className="row d-flex flex-column align-items-center ">
+                    <div className="col-8 mt-4">
+                        <div className="card" >
+                            <div className="card-body">
+                                <div className="row">
+                                <div className="col-8">
+                                <h5 className="card-title">Card title</h5>
+                                </div>
+                                <div className="col-4">
+                                    <div className="row">
+                                            <p>Date</p>
+                                    </div>
+                                    <div className="row">
+                                            <p>By User name</p>
+                                    </div>
+                                </div>
+                                </div>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-subtitle mb-2 text-muted">Status</h5>
+                                <hr/>
+                                <div className="row">
+                                    <p>By VC
+                                    </p>
+                                    <p>Action taken :-  In v4 we used a CSS-only technique to mimic the behavior of Masonry-like columns, but this technique came with lots of unpleasant side effects. If you want to have this type of layout in v5, you can just make use of Masonry plugin. Masonry is not included in Bootstrap, but we’ve made a demo example to help you get started.</p>
+                                    <p>Status :-  pending</p> 
+                                </div>
+                                <hr />
+                                <div className="row">
+                                    <p>By VC
+                                    </p>
+                                    <p>Action taken :-  In v4 we used a CSS-only technique to mimic the behavior of Masonry-like columns, but this technique came with lots of unpleasant side effects. If you want to have this type of layout in v5, you can just make use of Masonry plugin. Masonry is not included in Bootstrap, but we’ve made a demo example to help you get started.</p>
+                                    <p>Status :-  pending</p> 
+                                </div>
+                                <hr />
+                                <div className="row">
+                                    <p>By VC
+                                    </p>
+                                    <p>Action taken :-  In v4 we used a CSS-only technique to mimic the behavior of Masonry-like columns, but this technique came with lots of unpleasant side effects. If you want to have this type of layout in v5, you can just make use of Masonry plugin. Masonry is not included in Bootstrap, but we’ve made a demo example to help you get started.</p>
+                                    <p>Status :-  pending</p> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-8 mt-4">
+                        <div className="card" >
+                            <div className="card-body">
+                                <div className="row">
+                                <div className="col-8">
+                                <h5 className="card-title">Card title</h5>
+                                </div>
+                                <div className="col-4">
+                                    <div className="row">
+                                            <p>Date</p>
+                                    </div>
+                                    <div className="row">
+                                            <p>By User name</p>
+                                    </div>
+                                </div>
+                                </div>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-subtitle mb-2 text-muted">Status</h5>
+                                <hr/>
+                                <div className="row">
+                                    <p>By VC
+                                    </p>
+                                    <p>Action taken :-  In v4 we used a CSS-only technique to mimic the behavior of Masonry-like columns, but this technique came with lots of unpleasant side effects. If you want to have this type of layout in v5, you can just make use of Masonry plugin. Masonry is not included in Bootstrap, but we’ve made a demo example to help you get started.</p>
+                                    <p>Status :-  pending</p> 
+                                </div>
+                                <hr />
+                                <div className="row">
+                                    <p>By VC
+                                    </p>
+                                    <p>Action taken :-  In v4 we used a CSS-only technique to mimic the behavior of Masonry-like columns, but this technique came with lots of unpleasant side effects. If you want to have this type of layout in v5, you can just make use of Masonry plugin. Masonry is not included in Bootstrap, but we’ve made a demo example to help you get started.</p>
+                                    <p>Status :-  pending</p> 
+                                </div>
+                                <hr />
+                                <div className="row">
+                                    <p>By VC
+                                    </p>
+                                    <p>Action taken :-  In v4 we used a CSS-only technique to mimic the behavior of Masonry-like columns, but this technique came with lots of unpleasant side effects. If you want to have this type of layout in v5, you can just make use of Masonry plugin. Masonry is not included in Bootstrap, but we’ve made a demo example to help you get started.</p>
+                                    <p>Status :-  pending</p> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-8 mt-4">
+                        <div className="card" >
+                            <div className="card-body">
+                                <div className="row">
+                                <div className="col-8">
+                                <h5 className="card-title">Card title</h5>
+                                </div>
+                                <div className="col-4">
+                                    <div className="row">
+                                            <p>Date</p>
+                                    </div>
+                                    <div className="row">
+                                            <p>By User name</p>
+                                    </div>
+                                </div>
+                                </div>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-subtitle mb-2 text-muted">Status</h5>
+                                <hr/>
+                                <div className="row">
+                                    <p>By VC
+                                    </p>
+                                    <p>Action taken :-  In v4 we used a CSS-only technique to mimic the behavior of Masonry-like columns, but this technique came with lots of unpleasant side effects. If you want to have this type of layout in v5, you can just make use of Masonry plugin. Masonry is not included in Bootstrap, but we’ve made a demo example to help you get started.</p>
+                                    <p>Status :-  pending</p> 
+                                </div>
+                                <hr />
+                                <div className="row">
+                                    <p>By VC
+                                    </p>
+                                    <p>Action taken :-  In v4 we used a CSS-only technique to mimic the behavior of Masonry-like columns, but this technique came with lots of unpleasant side effects. If you want to have this type of layout in v5, you can just make use of Masonry plugin. Masonry is not included in Bootstrap, but we’ve made a demo example to help you get started.</p>
+                                    <p>Status :-  pending</p> 
+                                </div>
+                                <hr />
+                                <div className="row">
+                                    <p>By VC
+                                    </p>
+                                    <p>Action taken :-  In v4 we used a CSS-only technique to mimic the behavior of Masonry-like columns, but this technique came with lots of unpleasant side effects. If you want to have this type of layout in v5, you can just make use of Masonry plugin. Masonry is not included in Bootstrap, but we’ve made a demo example to help you get started.</p>
+                                    <p>Status :-  pending</p> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </>
     )
 }
